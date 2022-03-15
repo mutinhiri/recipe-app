2a3f5fd9ef1c0b97b24797f8b9c5627e74dd5aff
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-15 08:38:10 +0100

    Generate food controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,18 @@
+class FoodsController < ApplicationController
+  before_action :set_food, only: %i[show edit update destroy]
+  def index
+    
+  end
+  
+  private
+
+  def food_params
+    params.permit(:name, :measurement_unit, :price)
+  end
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+  
+  
+end
