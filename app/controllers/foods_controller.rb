2018ce6ff0a62b6aae0caa6fabb12bae601849f7
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-15 09:45:02 +0100

    Add food create method to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,6 +2,20 @@ class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
   def index; end
 
+  def create
+    @food = current_user.foods.create!(food_params)
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
+        format.json { render :show, status: :created, location: @food }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+
   private
 
   def food_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :foods
   get '/foods', to: 'foods#index', as: 'index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
