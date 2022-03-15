36919f0d34ec8964e893a891aa9a03ecdf77b249
Nicolae Pop <nicupop729@gmail.com>
2022-03-15 19:19:21 -0300

    Deliver linter check

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "foods#index"
+  root 'foods#index'
 
-  resources :inventories do 
+  resources :inventories do
     resources :inventory_foods
   end
-
 end
