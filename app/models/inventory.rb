6dfbfb1a97730531bb96ba2d07d9e2ac3ecd91c4
Nicolae Pop <nicupop729@gmail.com>
2022-03-16 23:31:40 -0300

    Add validations to model

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,4 +3,6 @@ class Inventory < ApplicationRecord
   has_many :inventory_foods, dependent: :destroy
   has_many :foods, through: :inventory_foods
   accepts_nested_attributes_for :inventory_foods, allow_destroy: true
+
+  validates :name, presence: true
 end
