af1cc5d8a74cb728f6a90c2b867e5c3ef0ed2fd1
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-17 22:30:59 +0200

    Add recipe foods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,12 +13,13 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     @foods = Food.all
-    # @recipe.recipe_foods.build
+    @recipe.recipe_foods.build
   end
 
   # GET /recipes/1/edit
   def edit
     @foods = Food.all
+    @recipe.recipe_foods.build
     @recipe.recipe_foods.find(params[:id])
     @food = @recipe.recipe_foods 
   end
@@ -33,6 +34,19 @@ class RecipesController < ApplicationController
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /recipes/1 or /recipes/1.json
+  def update
+    respond_to do |format|
+      if @recipe.update(recipe_params.merge(user: current_user))
+        format.html { redirect_to recipe_url(@recipe), success: 'Recipe was successfully updated.' }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @recipe.errors, status: :unprocessable_entity }
       end
     end
